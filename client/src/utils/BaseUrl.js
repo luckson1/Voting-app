6dbf9ede73b7381d61fb37abb6f37d1f9ac5b150
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-06-27 21:05:37 +0300

    build version
    build version

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
+
 export const BaseURL=  "https://votingke.herokuapp.com/api"
-=======
-export const BaseURL=  "https: //votingke.herokuapp.com/api"
->>>>>>> a90305d539e683b8d293d2ccf0ba02e4651deda6
